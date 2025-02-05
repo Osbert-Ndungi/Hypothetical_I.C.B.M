9830aec40cb64f1c7145fb061bae7924d3739afc
Osbert.Ndungi <osbertndungi@gmail.com>
2025-02-05 11:34:04 +0300

    Added ammonium perchlorate mass flow rate formula

--- main_acting_forces.py
+++ main_acting_forces.py
@@ -5,8 +5,8 @@ GRAVITATIONAL_CONSTANT = 6.67430 * 10**-11
 MASS_OF_EARTH = 5.972 * 10**24 #Mass of the Earth in kilograms
 RADIUS_OF_EARTH = 6.731 * 10**6 #Radius of then Earth in meters
 
-h = (float(input("What is the height of your rocket in metres: " )))
-m = (float(input("What is the mass of your rocket in kilograms: ")))
+h = (int(input("What is the height of your rocket in metres: " )))
+m = (int(input("What is the mass of your rocket in kilograms: ")))
 
 #The Gravitation formulae is : Gravitational_force(Fg) = (GRAVTITATIONAL_CONSTANT * mass_of_rocket * MASS_OF_EARTH) / (RADIUS_OF_EARTH + height_of_rocket)^2
 
@@ -26,8 +26,9 @@ print("-------------------------------------------------------------------------
 #Thrust-force
 # Based on the  joint formulaes of Newton , Hermann Oberth , Robert H. Goddard and Sergey Kororlev
 # The main foormulae is: Thrust-force(N) = mass_flow_rate_of_exhaust(kg/s) * exhaust_velocity + (nozzle_exit_pressure(Pa) - ambient_pressure(Pa)) * nozzle_exit_area(m^2)
-mass_flow_rate_propellant = (float(input("What is the mass flow rate of your propellant in kg/s (in decimal form): "))) #Is the mass of the propellant expelled per unit time
+mass_flow_rate_propellant = (int(input("What is the mass flow rate of your propellant in kg/s: "))) #Is the mass of the propellant expelled per unit time
 #exhaust_velocity = 
-rocket_fuel_type = (int(input("What type of propellant is being used:\n1.Solid\n2.Liquid\n3.Space-Engine e.g Ion-Thrusters")))
+rocket_fuel_type = (int(input("What type of propellant is being used:\n1.Solid\n2.Liquid\n3.Space-Engine e.g Ion-Thrusters\n")))
 if rocket_fuel_type == 1:
     solid_fuel_type = (int(input("What is your solid fuel type from the list below:\n1.Ammonium-Perchlorate\n2.Cast-Perchlorate\n3.Zinc-sulfur\n3.Composite e.g Solid-oxidizer + Binder + Metal-powder + Additive\n4.Double-base\n5.Black-powder\n6.")))
+if rocket_fuel_type == 2: (int(input("What is your liquid fuel type from the list below:\n1.Liquid hydrogen(LH2)\n2.Liquid Oxygen\n3.Hydrogen-Peroxide\n4.Petroleum-fuel\n5.Liquid-Methane\n6.Liquid-BioLPG")))
